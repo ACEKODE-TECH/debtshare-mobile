7656234628792df101fdfe389d5c77816913f356
Jorge Sanzo Hernando <jorgesanzo21@gmail.com>
2026-08-05 17:15:51 +0200

    chore(DEB-27): Setup develocity (#15)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,14 +12,32 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.develocity") version "4.5.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        publishing.onlyIf { System.getenv("CI") != null }
+        if (System.getenv("CI") != null) {
+            tag("CI")
+            tag(System.getenv("GITHUB_WORKFLOW"))
+            System.getenv("GITHUB_REF_NAME")?.let { tag(it) }
+
+            link("GitHub Actions Build", "https://github.com/${System.getenv("GITHUB_REPOSITORY")}/actions/runs/${System.getenv("GITHUB_RUN_ID")}")
+        }
     }
 }
 
